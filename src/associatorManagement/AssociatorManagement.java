83b9159addafac246d8b34c862434141471f36e3
Tianxin <925914554@qq.com>
2015-10-11 21:48:31 +0800

    会员和操作员接口新建

--- src/associatorManagement/AssociatorManagement.java
+++ src/associatorManagement/AssociatorManagement.java
@@ -3,11 +3,105 @@
  */
 package associatorManagement;
 
-/**会员管理模块
+import domain.Associator;
+import domain.Order;
+
+/**
+ * 会员管理模块
+ * 
  * @Description TODO
  * @author Tianxin
  * @date 2015年10月11日 下午7:47:45
  */
 public interface AssociatorManagement {
+	/**
+	 * 增加积分
+	 * 
+	 * @param associator
+	 *            会员
+	 * @param order
+	 *            订单
+	 * @return 成功标识
+	 */
+	public boolean addScore(Associator associator, Order order);
+
+	/**
+	 * 增加积分
+	 * 
+	 * @param associatorId
+	 *            会员id
+	 * @param order
+	 *            订单
+	 * @return
+	 */
+	public boolean addScore(String associatorId, Order order);
+
+	/**
+	 * 增加积分
+	 * 
+	 * @param associator
+	 *            会员
+	 * @param prize
+	 *            消费金额
+	 * @return
+	 */
+	public boolean addScore(Associator associator, double prize);
+
+	/**
+	 * 增加积分
+	 * 
+	 * @param associatorId
+	 *            会员id
+	 * @param prize
+	 *            消费金额
+	 * @return
+	 */
+	public boolean addScore(String associatorId, double prize);
+
+	/**
+	 * 扣除积分
+	 * 
+	 * @param associator
+	 * @param prize
+	 *            扣除金额
+	 * @return
+	 */
+	public boolean removeScore(Associator associator, double prize);
+
+	/**
+	 * 扣除积分
+	 * 
+	 * @param associatorId
+	 *            会员id
+	 * @param prize
+	 *            扣除金额
+	 * @return
+	 */
+	public boolean removeScore(String associatorId, double prize);
+
+	/**
+	 * 注册会员
+	 * 
+	 * @param associator
+	 * @return
+	 */
+	public boolean registerAssociator(Associator associator);
+
+	/**
+	 * 删除会员
+	 * 
+	 * @param associator
+	 * @return
+	 */
+	public boolean deleteAssociator(Associator associator);
+
+	/**
+	 * 根据id删除会员
+	 * 
+	 * @param associatorId
+	 *            会员id
+	 * @return
+	 */
+	public boolean deleteAssociator(String associatorId);
 
 }
--- src/operatorManagement/OperatorManagement.java
+++ src/operatorManagement/OperatorManagement.java
@@ -3,11 +3,100 @@
  */
 package operatorManagement;
 
-/**操作人员所有操作接口
+import domain.Clerk;
+import domain.Manager;
+import domain.Person;
+import domain.StockManager;
+
+/**
+ * 操作人员所有操作接口
+ * 
  * @Description TODO
  * @author Tianxin
  * @date 2015年10月11日 下午7:44:38
  */
 public interface OperatorManagement {
+	/**
+	 * 增加收银元
+	 * 
+	 * @param clerk
+	 * @return
+	 */
+	public boolean addClerk(Clerk clerk);
+
+	/**
+	 * 新增管理员
+	 * 
+	 * @param manager
+	 * @return
+	 */
+	public boolean addManagr(Manager manager);
+
+	/**
+	 * 新增库存管理员
+	 * 
+	 * @param stockManager
+	 * @return
+	 */
+	public boolean addStockManager(StockManager stockManager);
+
+	/**
+	 * 更改用户信息
+	 * 
+	 * @param person
+	 * @return
+	 */
+	public boolean changePersonInfo(Person person);
+
+	/**
+	 * 删除收银员
+	 * 
+	 * @param clerkId
+	 *            收银员id
+	 * @return
+	 */
+	public boolean removeClerk(String clerkId);
+
+	/**
+	 * 删除收银员
+	 * 
+	 * @param clerk
+	 * @return
+	 */
+	public boolean removeClerk(Clerk clerk);
+
+	/**
+	 * 删除管理员
+	 * 
+	 * @param managerId
+	 *            管理员id
+	 * @return
+	 */
+	public boolean removeManager(String managerId);
+
+	/**
+	 * 删除管理员
+	 * 
+	 * @param manager
+	 * @return
+	 */
+	public boolean removeManager(Manager manager);
+
+	/**
+	 * 删除库存管理员
+	 * 
+	 * @param managerId
+	 *            库存管理员id
+	 * @return
+	 */
+	public boolean removeStockManager(String managerId);
+
+	/**
+	 * 删除库存管理员
+	 * 
+	 * @param stockManager
+	 * @return
+	 */
+	public boolean removeStockManager(StockManager stockManager);
 
 }
